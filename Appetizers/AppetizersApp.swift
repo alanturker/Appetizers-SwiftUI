68fac8ac31a2ee9da4d2a03b341729bf493847f6
Turker Alan <turkeralan@Turker-MacBook-Air.local>
2024-12-23 15:38:39 +0300

    First Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Turker Alan on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
