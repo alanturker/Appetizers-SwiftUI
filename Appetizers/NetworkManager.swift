c34dd968069e340f4a74ae7049d4c19903af55fb
alanturker <alanturker@gmail.com>
2025-01-03 13:35:24 +0300

    #5 AppetizerRemoteImageView added. placeHolder image added and configured.

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Turker Alan on 24.12.2024.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared: NetworkManager = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -47,6 +48,33 @@ final class NetworkManager {
         }
         
         task.resume()
+    }
+    
+    func downloadImage(fromUrlString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data,
+                  let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
+        
     }
 
 }
